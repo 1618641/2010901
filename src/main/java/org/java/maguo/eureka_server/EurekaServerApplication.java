7bb2622aa8137d8b85c6c23ed1a8af25c1c97bb3
yuhonglinguit <2730869157@qq.com>
2019-01-09 15:48:11 +0800

    11

--- src/main/java/org/java/maguo/eureka_server/EurekaServerApplication.java
+++ src/main/java/org/java/maguo/eureka_server/EurekaServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
     //EEEEEEEEEEEEEEEEEE
+    //33333333333333333
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
